961203b8088d4798d47160df238f412fbb9067e8
Gaston <Gastongk@gmail.com>
2023-10-11 18:48:28 -0300

    se agrego Jquery para completar el ejercicio

--- js/script.js
+++ js/script.js
@@ -1,4 +1,30 @@
 $('a').click(function (desaparecer){
-    desaparecer.preventDefault(); //evisa que se redireccione al link
+    desaparecer.preventDefault(); //evita que se redireccione al link
     $(this).hide();
+});
+
+$('#cambiar').click(function () {
+    $('#contenido').html('<img src="src/esports.webp" alt="">'); // Nuevo contenido
+
+     // agregar css
+     $('#contenido img').css({
+       margin:'20px',
+        'max-width': '100%', 
+        height: 'auto', 
+    });
+});
+
+$('#addClase').on('click',function(){
+    $('ul li').addClass('claseNueva');
+});
+
+$('#btnToggle').on('click',function(){
+    $('#mostrar').toggle();
+    var buttonText = $(this).text();
+        
+        if (buttonText === "Mostrar") {
+            $(this).text("Ocultar");
+        } else {
+            $(this).text("Mostrar");
+        }
 });
